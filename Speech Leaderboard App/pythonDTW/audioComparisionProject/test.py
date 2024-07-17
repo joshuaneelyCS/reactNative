edb187d0b8ea39f7ffd7e27e40a9c841917bece6
Joshua Neely <54088141+joshuaneelyCS@users.noreply.github.com>
2024-07-16 18:31:17 -0600

    Update test.py

--- Speech Leaderboard App/pythonDTW/audioComparisionProject/test.py
+++ Speech Leaderboard App/pythonDTW/audioComparisionProject/test.py
@@ -2,18 +2,24 @@ import GiveScore
 
 # Provide two audio samples and compare similarities
 
+sample1 = "test_audio/Hello World (1).wav"
+sample2 = "test_audio/Hello World (1.5).wav"
+sample3 = "test_audio/Hello World (2).wav"
+sample4 = "test_audio/Hello World (3).wav"
+sample5 = "test_audio/Dummy Recording.wav"
+
 # Returns a distance of 0 because they are identical and there are no differences
-print(GiveScore.give_score("Hello World (1).wav","Hello World (1).wav"))
+print(GiveScore.give_score(sample1, sample1)
 
 # Returns a greater distance because they are similar but not identical
-print(GiveScore.give_score("Hello World (1).wav","Hello World (1.5).wav"))
+print(GiveScore.give_score(sample1, sample2))
 
 # Returns a greater distance than the previous because they are less similar
-print(GiveScore.give_score("Hello World (1).wav","Hello World (2).wav"))
+print(GiveScore.give_score(sample1, sample3))
 
 # Returns a greater distance than the previous because they are no longer similar
-print(GiveScore.give_score("Hello World (1).wav","Hello World (3).wav"))
+print(GiveScore.give_score(sample1, sample4))
 
 # Returns the largest distance because I am no longer saying the same words
-print(GiveScore.give_score("Hello World (1).wav","Dummy Recording.wav"))
+print(GiveScore.give_score(sample1, sample5))
 
